8629763f42f70c7b30f55eae64a8618cd9be5cc8
schirhei <31994547+schirhei@users.noreply.github.com>
2018-05-24 19:36:49 -0700

    use env variables

--- databaseCred.js
+++ databaseCred.js
@@ -1,7 +1,4 @@
-var uri = "mongodb+srv://mongodb-stitch-europeana-bdhxh:whydoesntmongodbwork@europeanaimaging-porog.mongodb.net/Users?retryWrites=true";
-var apik = 'BScfR482T'
-
 module.exports = {
-	apik,
-    uri
+	apik: process.env.api,
+    uri: process.env.uri
 };
