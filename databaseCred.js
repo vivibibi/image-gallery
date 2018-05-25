56d3d1163d434ff5fc10d2414749422c6804ddab
schirhei <31994547+schirhei@users.noreply.github.com>
2018-05-25 16:25:15 +0000

    Update databaseCred.js

--- databaseCred.js
+++ databaseCred.js
@@ -1,2 +1,4 @@
-var uri = "mongodb+srv://mongodb-stitch-europeana-bdhxh:whydoesntmongodbwork@europeanaimaging-porog.mongodb.net/Users?retryWrites=true";var apik = 'BScfR482T' 
-module.exports = {	apik,    uri };
+module.exports = {
+	apik: process.env.api,
+    uri: process.env.uri
+};
